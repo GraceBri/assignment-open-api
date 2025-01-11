3af58bee25eb18060cf31c2d73e1085ab6b788c5
Your Name <cjvictoria24@gmail.com>
2025-01-11 16:41:07 +0800

    search user

--- src/instagram/instagram.controller.ts
+++ src/instagram/instagram.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
+import { InstagramService } from './instagram.service';
 
 @Controller('instagram')
-export class InstagramController {}
+export class InstagramController {
+  constructor(private readonly instagramService: InstagramService) {}
+
+  /**
+   * Endpoint to search for Instagram users based on a query string.
+   * @param query The query string to search users.
+   * @returns The search results.
+   */
+  @Get('search-users')
+  async searchUsers(@Query('query') query: string) {
+    if (!query) {
+      throw new BadRequestException('Query parameter is required');
+    }
+    return await this.instagramService.searchUsers(query);
+  }
+
+}
+
